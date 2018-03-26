8c240f0205dc9592073b2de163464e38913e46c4
Victor Requena <svictoreq@gmail.com>
2018-03-26 18:06:46 -0400

    Add not found component

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './Home';
+import NotFound from './NotFound';
 
 class App extends Component {
 	render() {
@@ -8,6 +9,7 @@ class App extends Component {
 			<Router>
 				<Switch>
 					<Route exact path="/" component={Home} />
+					<Route component={NotFound} />
 				</Switch>
 			</Router>
 		);
---
+++ src/components/NotFound.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../css/NotFound.css';
+
+const NotFound = (props) => {
+	return (
+		<div className="not-found">
+			<div className="wrapper">
+				<h1>Sorry, not found...</h1>
+
+			</div>
+		</div>
+	);
+};
+
+export default NotFound;
